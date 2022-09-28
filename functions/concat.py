bae5faa85a80168ec5edbe028db763b5971f3dd2
LoaiEsam37 <114489561+LoaiEsam37@users.noreply.github.com>
2022-09-28 09:14:22 +0000

    Create concat.py

---
+++ functions/concat.py
@@ -0,0 +1,66 @@
+import re
+import pandas as pd
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+from itertools import combinations
+from collections import Counter
+from colorama import Fore
+import sys
+import getpass
+
+def MyFunc(DIR, OUTPUT):
+    if DIR == "":
+        path = os.getcwd()
+    else:
+        path = os.getcwd()+DIR
+    files = [file for file in os.listdir(path) if not file.startswith('.') and re.search(".csv", file) and not file == OUTPUT] # Ignore hidden files
+
+    all_months_data = pd.DataFrame()
+
+    print("\n")
+    for file in files:
+        try:
+            current_data = pd.read_csv(path+"/"+file)
+            all_months_data = pd.concat([all_months_data, current_data])
+            print(f"{Fore.LIGHTGREEN_EX}+{Fore.WHITE} {file}")
+        except:
+            pass
+
+    all_months_data.to_csv(OUTPUT, index=False)
+
+def USER_INPUT():
+    # DIR
+    while True:
+        print(f"{Fore.LIGHTGREEN_EX}Type Directory(Input)")
+        USER = input(f"{Fore.WHITE}{getpass.getuser()}@DataParse$ ")
+
+        try:
+            DIR = os.path.join("/", USER)
+            test = os.getcwd()+DIR
+            os.listdir(test)
+            break
+        except:
+            print(f"{Fore.RED}[ ! ] invaild, Try again")
+    # OUTPUT
+    while True:
+        print(f"{Fore.LIGHTGREEN_EX}filename(Output)")
+        USER = input(f"{Fore.WHITE}{getpass.getuser()}@DataParse$ ")
+
+        try:
+            if re.search(".csv", USER):
+                f = open(f"{os.getcwd()}/{USER}", "a")
+                OUTPUT = USER
+                break
+            else:
+                f = open(f"{os.getcwd()}/{USER}.csv", "a")
+                OUTPUT = USER + ".csv"
+                break
+        except:
+            print(f"{Fore.RED}[ ! ] invaild, Try again")
+
+    return DIR, OUTPUT
+
+def Easy_Option():
+    DIR, OUTPUT = USER_INPUT()
+    MyFunc(DIR, OUTPUT)
